f25aa8918d2c110a902202776b57afa6d8033546
Kazi Islam <kazichaska@gmail.com>
2022-01-21 22:01:37 -0600

    added function and modularize it

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+console.log(process);
+const [name, github] = profileDataArgs;
 
 
-// var message = 'Hello Node!';
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-// var sum = 5 + 3;
-
-// console.log(message);
-// console.log(sum);
-// console.log(process);
-
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-// arrow function with no parameter so removed () from profileDataArr
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
