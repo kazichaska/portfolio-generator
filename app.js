c5294a114af40ebbf37a7baa0195590f08b5dc4f
Kazi Islam <kazichaska@gmail.com>
2022-01-22 18:44:13 -0600

    inquirer package

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
-console.log(process);
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// console.log(inquirer);
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
